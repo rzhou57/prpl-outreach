2e383d125f4fb327be692022ca71a8bd2b019d88
Tomasz <tsadowy@princeton.edu>
2026-04-27 23:19:15 -0400

    concurrent heroku backends

--- bin/start.sh
+++ bin/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Start kinder-blockly on its internal port, in the background
-python -m kinder_blockly --port 5000 &
+cd server
 
-# Start Node (binds to $PORT for Heroku)
-cd server && npm start
+# Start both Node and Python concurrently
+npx concurrently -n server,blockly \
+  "npm start" \
+  "python -m kinder_blockly --port 5000"
